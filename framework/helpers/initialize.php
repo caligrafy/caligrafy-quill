e5b3a3ff817ff6f7095cac9980105a77494d794c
Dory Azar <dory.azar@gmail.com>
2022-09-12 18:35:51 -0400

    Updating sandbox server

--- framework/helpers/initialize.php
+++ framework/helpers/initialize.php
@@ -41,8 +41,8 @@ define("FONTS_PATH", PUBLIC_PATH . "fonts" . DS);
 
 
 //URL of the application
-$serverName = getenv(SANDBOX_SERVER)?? (isset($_SERVER['SERVER_NAME'])? $_SERVER['SERVER_NAME'] : '');
-$port =  getenv(SANDBOX_PORT)?? (isset($_SERVER['SERVER_PORT']) && ':'.$_SERVER['SERVER_PORT'] != '80'  ? ':'.$_SERVER['SERVER_PORT'] : '');
+$serverName = getenv('APP_ENV') == 'sandbox'? getenv('SANDBOX_SERVER') : (isset($_SERVER['SERVER_NAME'])? $_SERVER['SERVER_NAME'] : '');
+$port =  getenv('APP_ENV') == 'sandbox'? getenv('SANDBOX_PORT') : (isset($_SERVER['SERVER_PORT']) && ':'.$_SERVER['SERVER_PORT'] != '80'  ? ':'.$_SERVER['SERVER_PORT'] : '');
 $_SESSION['base'] = getenv('APP_PROTOCOL')."://".$serverName.$port.DS;
 $_SESSION['home'] = $_SESSION['base'].(getenv('APP_ROOT')? getenv('APP_ROOT').DS : '');
 $_SESSION['public'] = $_SESSION['home'].'public'.DS;
