e994596f3679f2f0b37127b3376ee54a4b2e7122
Dory Azar <dory.azar@gmail.com>
2022-09-29 20:51:38 -0400

    Fixing Vue app home

--- framework/librairies/app/src/components/pages/HomePage.vue
+++ framework/librairies/app/src/components/pages/HomePage.vue
@@ -6,32 +6,67 @@
 
 <template>
     <div id='vue-home'>
-		<div class='spacer-2'></div>
-		<show-message message="Welcome to Caligrafy"></show-message>
+		<div class="spacer-4"></div>
+		<show-message header="Welcome to Caligrafy" message="Start from the ground up with this light MVC framework"></show-message>
+		<div class="spacer-4"></div>
+		<template v-for="homecard in homecards" :key="homecard.id">
+			<div class="col-sm-6 col-md-4">
+				<show-card :homecard="homecard" ></show-card>
+			</div>
+		</template>
 		<div class='spacer-2'></div>
     </div>
 </template>
 
 <script>
 import ShowMessage from '@/components/global/ShowMessage.vue';
+import ShowGeneralCard from '@/components/global/ShowGeneralCard.vue';
 
 export default {
     name: 'vue-home',
     components: {
-        'show-message': ShowMessage
+		'show-message': ShowMessage,
+		'show-card': ShowGeneralCard
     },
 	data() {
-		return {}
+		return {
+			homecards:  [
+					{
+						"id": 1,
+						"link":"https://github.com/caligrafy/caligrafy-quill/wiki/9.8.2-Routing-for-Single-Page-Applications#mainjs",
+						"title": "main.js",
+						"description": "It all starts in this file where you define the routes, styles, keys and other settings"
+
+					},
+					{
+						"id": 2,
+						"link":"https://github.com/caligrafy/caligrafy-quill/wiki" ,
+						"title": "Docs and Cookbook",
+						"description": "Learn the ins and outs of the Caligrafy Quill framework"
+
+					},
+					{
+						"id": 3,
+						"link": "https://youtube.com/playlist?list=PLsJG81fLk3_VWmXl9DnEldYY9ei4jTCjA",
+						"title": "Caligrafy Channel",
+						"description": "Watch our video tutorials to learn how to build powerful and sophisticated features"
+					},
+					{
+						"id": 4,
+						"link": "https://www.udemy.com/courses/search/?src=ukw&q=caligrafy" ,
+						"title": "Caligrafy Academy",
+						"description": "We offer detailed hands-on online courses that will progressively make you an expert in Caligrafy"
+					}
+				]
+		}
 	},
 	methods: {
-		// define methods
 	},
 	computed: {
 		// computed variables
 	},
 	
 	mounted() {
-		// execute methods and actions upon mounting
 	},
 	watch: {
 		// watched properties
