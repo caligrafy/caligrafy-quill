2d53e6e398d598fca179059faeaf31e9ba1884d6
Dory Azar <dory.azar@gmail.com>
2022-11-10 10:34:40 -0500

    Fixing facedetect library

--- framework/plugins/facedetect/scripts/main.js
+++ framework/plugins/facedetect/scripts/main.js
@@ -17,11 +17,11 @@ const app = Vue.createApp({
         this.detector.loadApp(app);
       },
       
-      continuousMethod: function(facedetector) {
-          console.log(facedetector.app.detections);
+      continuousMethod: function() {
+          console.log(this.detector.app.detections);
       },
       
-      callbackMethod: function(facedetector) {
+      callbackMethod: function() {
          
         //<---- do whatever you want here
           console.log('Example of what can be done');
@@ -29,40 +29,39 @@ const app = Vue.createApp({
       
       
       /* FaceDetect methods and properties
-        * facedetector.app.detections: returns a stream of detections or recognitions. You can iterate through them to act on them
-        * facedetector.app.canvas: returns the canva that overlays the video
-        * facedetector.app.options: exposes all the options of the app
+        * this.detector.app.detections: returns a stream of detections or recognitions. You can iterate through them to act on them
+        * this.detector.app.canvas: returns the canva that overlays the video
+        * this.detector.app.options: exposes all the options of the app
         * 
-        * facedetector.media: exposes the media used (video, picture, cam stream)
+        * this.detector.media: exposes the media used (video, picture, cam stream)
         *             .media.width: gets the width
         *             .media.height: gets the height
         * 
-        * facedetector.loadApp(app): load another app
-        * (facedetector.detectFaces(app, facedetector))(): self invoking function to start face detection
-        * facedetector.detect(callback, recognize = false, fetchRate = 100): starts a parallel stream that captures any detections or recognitions when available
-        * facedetector.prepareCanva(options = null): returns a new canvas on top of the media source
-        * facedetector.draw(facedetector): draws the detections on the canvas
-        * facedetector.loadRecognition({ labels: [], images: [], sampleSize: 100}): load models to recognize by the recognition engine
-        * facedetector.recognize(facedetector): runs the recognition engine and draws on canvas. Must make sure that detections is started before
-        * facedetector.fetchImage(canvas, media): takes a canvas capture of the media and returns a blob data image (data url)
-        * facedetector.display(message, output): displays a message in the infobar and gives it an ID as specified by the 'output' input
-        * facedetector.clearDisplay(): clears the infobar display
-        * facedetector.clearCanva(canvas): clears the canva
+        * this.detector.loadApp(app): load another app
+        * (this.detector.detectFaces(app, facedetector))(): self invoking function to start face detection
+        * this.detector.detect(callback, recognize = false, fetchRate = 100): starts a parallel stream that captures any detections or recognitions when available
+        * this.detector.prepareCanva(options = null): returns a new canvas on top of the media source
+        * this.detector.draw(facedetector): draws the detections on the canvas
+        * this.detector.loadRecognition({ labels: [], images: [], sampleSize: 100}): load models to recognize by the recognition engine
+        * this.detector.recognize(facedetector): runs the recognition engine and draws on canvas. Must make sure that detections is started before
+        * this.detector.fetchImage(canvas, media): takes a canvas capture of the media and returns a blob data image (data url)
+        * this.detector.display(message, output): displays a message in the infobar and gives it an ID as specified by the 'output' input
+        * this.detector.clearDisplay(): clears the infobar display
         */
 
       /* FACEAPI methods and properties
        *
        * Properties of every detection: detection.box, age, expression, gender, landmarks, descriptors
        *
-       * (new faceapi.draw.DrawFaceLandmarks(landmark, {settings})).draw(canva): function that draws the face landmark. Landmarks are attributes of the detections like age, gender etc.
+       * faceapi.draw.DrawFaceLandmarks(landmark, {settings})).draw(canva): function that draws the face landmark. Landmarks are attributes of the detections like age, gender etc.
                 * draw landmark settings : drawLines (boolean), drawPoints (boolean), lineWidth (number), lineColor (rgba(x, x, x, x)), pointSize(number), pointColor(rgba(x, x, x, x))
        * 
-       * faceapi.draw.drawDetections(canva, detections): Draws the detections on the canva
+       * faceapi.draw.DrawDetections(canva, detections): Draws the detections on the canva
        * 
-       * (new faceapi.draw.DrawTextField(Array of strings to display, anchor)).draw(canva): Draws text on the canva around the detections
+       * faceapi.draw.DrawTextField(Array of strings to display, anchor).draw(canva): Draws text on the canva around the detections
        *        * anchor: where the text should be placed at every detection. For example it could be: detection.detection.box.bottomLeft
        * 
-       * (new faceapi.draw.DrawBox(box, {settings})).draw(canva): draws a box around the face 
+       * faceapi.draw.DrawBox(box, {settings}).draw(canva): draws a box around the face 
        * 
        * faceapi.matchDimensions(canva, {width: media width, height: media height}): changes the dimensions of the canva to match the media
        * faceapi.resizeResults(detections, {width: media width, height: media height}): resizes the detections to fit within the video
